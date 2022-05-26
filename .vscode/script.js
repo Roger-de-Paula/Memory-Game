7f84f592f5dccf2fbe0f1944a89abf20f7f6bc7b
Roger_Tech <rogerio.gss123@gmail.com>
2022-05-26 19:11:16 -0300

    unflip function fixed

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,42 +1,46 @@
-const cards =  document.querySelectorAll(".card");
+const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
 
+//flip the card
 function flipCard() {
     if(lockBoard) return;
     if(this === firstCard) return;
 
-    this.classList.add("flip")
+    this.classList.add('flip');
     if(!hasFlippedCard) {
-        hasFlippedCard = True;
+        hasFlippedCard = true;
         firstCard = this;
         return;
     }
+
     secondCard = this;
     hasFlippedCard = false;
-    checkForMath();
-
+    checkForMatch();
 }
 
-function checkForMath(){
+//check if the cards match
+function checkForMatch() {
     if(firstCard.dataset.card === secondCard.dataset.card) {
         disableCards();
         return;
     }
+
     unflipCards();
 }
 
+//disable the cards
 function disableCards() {
-    firstCard.removeEventListener('click', flipCard());
-    secondCard.removeEventListener('click', flipCard());
-    resetBoard();
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
 
+    resetBoard();
 }
 
+//unflip the cards
 function unflipCards() {
     lockBoard = true;
-
 
     setTimeout(() => {
         firstCard.classList.remove('flip');
@@ -46,13 +50,21 @@ function unflipCards() {
     }, 1500);
 }
 
+//reset
 function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
 
+//mix the cards
+(function shuffle() {
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPosition;
+    })
+})();
 
+//cards click event
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 });
-
